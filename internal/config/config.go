564ec4041b9ecb3d0cdf26e3df0635c212fcfd1e
Jacob McSwain <jacob@mcswain.dev>
2025-05-28 10:26:57 -0500

    config: 4x default scale

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,7 @@ package config
 
 type Config struct {
 	LogLevel   LogLevel `name:"log-level" description:"Logging level for the application. One of debug, info, warn, or error" default:"info"`
-	Scale      float64  `name:"scale" description:"Scale factor for the display." default:"2.0"`
+	Scale      float64  `name:"scale" description:"Scale factor for the display." default:"4.0"`
 	Fullscreen bool     `name:"fullscreen" description:"Enable fullscreen mode."`
 	ROM        string   `name:"rom" description:"Path to the ROM file to load."`
 	BIOS       string   `name:"bios" description:"Path to the BIOS file to load."`
